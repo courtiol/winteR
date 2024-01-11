b6d3e3b176dd9492568c4e3dd7e4625d22c6d625
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2024-01-11 10:08:16 +0100

    Fix bug mirroring and create new plot

--- R/budget.R
+++ R/budget.R
@@ -1,8 +1,9 @@
 #' Compute the energy budget during a winter
 #'
-#' Bugets are computed in KJ per hour and grams of fat consumed. If `data_MR` contains a column
-#' `Date`, the function will also compute the cumulative budget during the hibernation season and
-#' the predicted survival status.
+#' Bugets are computed in KJ and grams of fat consumed per hour (`MR_normo`, `MR_torpor`,
+#' `Fat_normo`, `Fat_torpor`) and per day (`Budget_MR` and `Budget_fat`). If `data_MR` contains a
+#' column `Date`, the function will also compute the cumulative budget during the hibernation season
+#' and the predicted survival status.
 #'
 #' @inheritParams arguments
 #'
@@ -14,22 +15,34 @@
 #'
 compute_budget_df <- function(data_MR, fit_state, fit_MR, roost_insulation_dTa = 5,
                               temp_threshold = 7, split_summer = "07-01", min_days_trigger_winter = 14,
-                              threshold_mortality = 27, huddling_factor = 0.5) {
+                              threshold_mortality = 27, huddling_factor = 0.5, Tmirror = 2) {
 
   ## ambient temperature is higher in roost
   data_MR$Ta <- data_MR$Temp + roost_insulation_dTa
+
+  ## ambient temperature to use for predictions of physiological state (i.e. with mirroring)
+  data_MR$Ta_mirrored <- pmax(data_MR$Ta, -data_MR$Ta + 2*Tmirror)
 
   # print(data_MR$Ta[1]) ## for debugging only
   # if (abs(data_MR$Ta[1] -  17.92367) < 1e-6) browser()
 
   ## predict proba to be in normothermy and in torpor
-  prob_normo <- stats::predict(fit_state, newdata = data.frame(Ta = data_MR$Ta), re.form = NA)[,1 ]
+  prob_normo <- stats::predict(fit_state, newdata = data.frame(Ta = data_MR$Ta_mirrored), re.form = NA)[,1 ]
   prob_torpor <- 1 - prob_normo
   data_MR$Prob_normo <- prob_normo
   data_MR$Prob_torpor <- prob_torpor
 
   ## predict metabolic rate in both physiological states
   pred_MR <- suppressWarnings(torpor::tor_predict(fit_MR, Ta = data_MR$Ta, CI = FALSE))
+  pred_MR_noMTNZ <- pred_MR[pred_MR$assignment != "Mtnz", ]
+  pred_MR_MTNZ <- pred_MR[pred_MR$assignment == "Mtnz", ]
+
+  if (nrow(pred_MR_MTNZ) > 0) {
+    pred_MR_MTNZ_torpor <- pred_MR_MTNZ_normo <- pred_MR_MTNZ
+    pred_MR_MTNZ_torpor$assignment <- "Torpor"
+    pred_MR_MTNZ_normo$assignment <- "Euthermia"
+    pred_MR <- rbind(pred_MR_noMTNZ, pred_MR_MTNZ_torpor, pred_MR_MTNZ_normo)
+  }
 
   # params <- torpor::get_parameters(fit_MR)
   # Tlc <- params[params$parameter == "Tlc", "median"]
@@ -39,14 +52,14 @@ compute_budget_df <- function(data_MR, fit_state, fit_MR, roost_insulation_dTa =
   data_MR$MR_normo <- NA
 
   data_MR$MR_normo[data_MR$Ta < Tlc] <- pred_MR[pred_MR$Ta < Tlc & pred_MR$assignment == "Euthermia", "pred"] * huddling_factor
-  data_MR$MR_normo[data_MR$Ta >= Tlc] <- pred_MR[pred_MR$Ta >= Tlc & pred_MR$assignment == "Mtnz", "pred"] # no change for huddling needed here
+  data_MR$MR_normo[data_MR$Ta >= Tlc] <- pred_MR_MTNZ[pred_MR_MTNZ$Ta >= Tlc & pred_MR_MTNZ$assignment == "Mtnz", "pred"] # no change for huddling needed here
 
-  data_MR$MR_torpor <- 0
-  data_MR$MR_torpor[data_MR$Ta < Tlc & data_MR$Ta >= Tt] <- pred_MR[pred_MR$Ta < Tlc & pred_MR$Ta >= Tt & pred_MR$assignment == "Torpor", "pred"] # no change for huddling needed here
-  data_MR$MR_torpor[data_MR$Ta < Tlc & data_MR$Ta < Tt] <- pred_MR[pred_MR$Ta < Tlc & pred_MR$Ta < Tt & pred_MR$assignment == "Torpor", "pred"] * huddling_factor
+  data_MR$MR_torpor <- NA
+  data_MR$MR_torpor[data_MR$Ta >= Tt] <- pred_MR[pred_MR$Ta >= Tt & pred_MR$assignment == "Torpor", "pred"] # no change for huddling needed here
+  data_MR$MR_torpor[data_MR$Ta < Tt] <- pred_MR[pred_MR$Ta < Tt & pred_MR$assignment == "Torpor", "pred"] * huddling_factor
 
-  data_MR$Prob_normo[data_MR$Ta >= Tlc] <- 1
-  data_MR$Prob_torpor[data_MR$Ta >= Tlc] <- 0
+  # data_MR$Prob_normo[data_MR$Ta_mirrored >= Tlc] <- 1
+  # data_MR$Prob_torpor[data_MR$Ta_mirrored >= Tlc] <- 0
 
   ## compute budget
   data_MR$Fat_normo <- data_MR$MR_normo/37.7
@@ -54,8 +67,11 @@ compute_budget_df <- function(data_MR, fit_state, fit_MR, roost_insulation_dTa =
 
   data_MR$Daily_duration_normo <- data_MR$Prob_normo * 24
   data_MR$Daily_duration_torpor <- data_MR$Prob_torpor * 24
-  data_MR$Budget_MR <- data_MR$MR_normo * data_MR$Daily_duration_normo + data_MR$MR_torpor * data_MR$Daily_duration_torpor
+  data_MR$Budget_MR_normo <- data_MR$MR_normo * data_MR$Daily_duration_normo
+  data_MR$Budget_MR_torpor <- data_MR$MR_torpor * data_MR$Daily_duration_torpor
+  data_MR$Budget_MR <- data_MR$Budget_MR_normo + data_MR$Budget_MR_torpor
   data_MR$Budget_fat <- data_MR$Fat_normo * data_MR$Daily_duration_normo + data_MR$Fat_torpor * data_MR$Daily_duration_torpor
+  data_MR$Budget_fat_assumingcontinuoustorpor <- data_MR$Fat_torpor * 24
 
   ## if date provided, compute cumulative budget and survival
   if (!is.null(data_MR$Date)) {
@@ -177,12 +193,13 @@ plot_budget_panel <- function(data_budget, y = "g_fat_per_winter", threshold_mor
   } else if (y == "g_fat_per_day") {
     ylab <- expression(atop("Daily fat consumption"~"("*g*.*day^{-1}*")"))
     plot <- plot +
+      ggplot2::geom_line(ggplot2::aes(x = .data$Date, y = .data$Budget_fat_assumingcontinuoustorpor), colour = "blue") +
       ggplot2::geom_line(ggplot2::aes(x = .data$Date, y = .data$Budget_fat)) +
       ggplot2::scale_y_continuous(breaks = seq(0, 10, by = 0.25), minor_breaks = NULL)
   } else if (y == "g_fat_per_winter") {
     date_death <- data_plot$Date[which(data_plot$Budget_cumul > threshold_mortality)[1]]
     print(paste("death date =", date_death))
-    ylab <- expression(atop("Cumulative fat consumption"~"("*g*.*Sigma*day^{-1}*")"))
+    ylab <- expression(atop("Cumulative fat consumption"~"("*g*.*Sigma*" "*day^{-1}*")"))
     plot <- plot +
       ggplot2::geom_hline(yintercept = threshold_mortality, linetype = 4, colour = "darkgrey") +
       ggplot2::geom_line(ggplot2::aes(x = .data$Date, y = .data$Budget_cumul), data = data_plot[data_plot$Budget_cumul < threshold_mortality, ]) +
@@ -203,6 +220,50 @@ plot_budget_panel <- function(data_budget, y = "g_fat_per_winter", threshold_mor
     ggplot2::labs(y = ylab, x = NULL) +
     ggplot2::theme_bw(base_size = base_size) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(hjust = 0), plot.margin = ggplot2::margin(l = 15))
+}
+
+
+#' Plot budget curves
+#'
+#' @inheritParams arguments
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' data("fit_torpor", package = "winteR")
+#' data("fit_normo_cauchit", package = "winteR")
+#' plot_budget_curves(fit_state = fit_normo_cauchit, fit_MR = fit_torpor)
+#'
+plot_budget_curves <- function(fit_state, fit_MR, rangeTa = c(-15, 35), Tmirror = 2, base_size = 11) {
+
+  xlab <- "Ambient temperature (\u00B0C)"
+  ylab <- expression(atop("Daily fat consumption"~"("*g*.*day^{-1}*")"))
+
+  budg <- compute_budget_df(data.frame(Temp = seq(rangeTa[1], rangeTa[2], by = 0.01)),
+                                       roost_insulation_dTa = 0, # note remove insulation
+                                       fit_state = fit_state,
+                                       fit_MR = fit_MR, huddling_factor = 1)
+
+  budg <- budg[, c("Ta", "Budget_fat", "Budget_fat_assumingcontinuoustorpor")]
+  budg_long <- tidyr::pivot_longer(budg, cols = c("Budget_fat", "Budget_fat_assumingcontinuoustorpor"))
+  budg_long$name <- factor(budg_long$name, levels = c("Budget_fat_assumingcontinuoustorpor", "Budget_fat"))
+
+  par <- torpor::get_parameters(fit_MR)
+  Tt <- par[par$parameter == "Tt", "mean"]
+
+  ggplot2::ggplot(budg_long) +
+    ggplot2::geom_vline(xintercept = Tt, linetype = 2) +
+    ggplot2::geom_line(ggplot2::aes(x = .data$Ta, y = .data$value, colour = .data$name)) +
+    ggplot2::labs(y = ylab, x = NULL, colour = "State") +
+    ggplot2::scale_x_continuous(breaks = seq(rangeTa[1], rangeTa[2], by = 5), minor_breaks = NULL) +
+    ggplot2::scale_y_continuous(breaks = seq(0, 10, by = 0.5), minor_breaks = NULL) +
+    ggplot2::scale_colour_manual(values = c("blue", "black")) +
+    ggplot2::theme_bw(base_size = base_size) +
+    ggplot2::theme(#plot.margin = ggplot2::margin(l = 15),
+                   legend.position = "none") +
+    ggplot2::xlab(xlab) +
+    ggplot2::ylab(ylab)
 }
 
 
